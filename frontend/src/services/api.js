7e9a284d23b02b53127ec2ee060a08d30cfacac6
shettysatvikj <shettysatvik@gmail.com>
2026-02-15 23:58:18 +0530

    Fixed axios baseURL correctly

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://grocery-del-backend1.onrender.com",
 });
+
 // ✅ Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
